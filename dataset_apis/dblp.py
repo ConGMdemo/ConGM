cc9b9d03e8946b41b3cae14b8318bd94e3e6b0ed
ConGMdemo <2821026975@qq.com>
2025-05-24 00:44:31 +0800

    first commit

--- dataset_apis/dblp.py
+++ dataset_apis/dblp.py
@@ -1,6 +1,7 @@
 from torch_geometric.datasets import CitationFull
 import torch_geometric.transforms as T
 
+
 def load_trainset(trans):
     dataset = CitationFull(root='~/datasets', name='dblp', transform=T.Compose([trans]))
     return dataset
